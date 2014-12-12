fecf7aba6dab2579b1643e6ab454473140b2d3a8
theoblivious <hashimoto.kenn@gmail.com>
2014-12-12 14:28:19 -0500

    changed env labeling

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['1501983380090676'], ENV['4ba1d21073a3330a54374466484937de'],
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
     scope: 'public_profile', display: 'page', image_size: 'square'
 end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['R1t4dMw8f8p9VKAguAWXDM1UV'], ENV['i5WRY9IGKIgfPGadNpbKdPF46vTsGoN0Zm6i2QsMC2Ca8hOMuL'], image_size: 'normal'
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET'], image_size: 'normal'
 end
