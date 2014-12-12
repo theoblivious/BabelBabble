5bbd2dea1178b81690ccb53b90b4ff8280357790
theoblivious <hashimoto.kenn@gmail.com>
2014-12-12 11:36:45 -0500

    testing front page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'sessions/auth_fail'
+
+get '/auth/:provider/callback', to: 'sessions#create'
+get '/auth/failure', to: 'sessions#auth_fail'
+get '/sign_out', to: 'sessions#destroy', as: :sign_out
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'sessions#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
