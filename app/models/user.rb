7cc072f3a22e21b9169d0b80e9093b752a9f71c3
theoblivious <hashimoto.kenn@gmail.com>
2014-12-12 13:38:35 -0500

    updated model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
-end
+  class << self
+    def from_omniauth(auth)
+      provider = auth.provider
+      uid = auth.uid
+      info = auth.info.symbolize_keys!
+      user = User.find_or_initialize_by(uid: uid, provider: provider)
+      user.name = info.name
+      user.avatar_url = info.image
+      user.profile_url = info.urls.send(provider.capitalize.to_sym)
+      user.save!
+      user
+    end
+  end
+ end
