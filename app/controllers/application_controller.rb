a1be773b02729d127e8e5134672e7dede387b341
theoblivious <hashimoto.kenn@gmail.com>
2014-12-15 13:47:40 -0500

    ensure authenticated in all actions
    Allows anon in development

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,31 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :authenticate_user
 
   protected
+
+  def authenticate_user
+    if cookies[:user_id]
+      self.current_user = User.find(cookies[:user_id])
+    end
+
+    # TODO: Move to someting like
+    # app_requires_auth = Rails.config.requires_authentication
+    app_requires_auth = !Rails.env.development?
+
+    unless app_requires_auth
+      # use anonymous user to support current_user interface when not logged in.
+      anonymous_user = OpenStruct.new(name: 'Anonymous')
+      self.current_user ||= anonymous_user
+    end
+
+    redirect_to login_url, notice: "You must be logged in to access." unless current_user
+  end
+
 # this makes it available to all of our controllers since everything inherits from Application Controller
   def current_user
-    # use anonymous user to support current_user interface when not logged in.
-    anonymous_user = OpenStruct.new(name: 'Anonymous')
-    @current_user ||= anonymous_user
+    @current_user
   end
 
   def current_user=(value)
