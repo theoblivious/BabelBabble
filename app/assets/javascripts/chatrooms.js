7745d7c2953a100ceb7a84d5e054051df226b028
theoblivious <hashimoto.kenn@gmail.com>
2014-12-17 14:06:49 -0500

    refactoring testing of javascript. moved javascript file to my chatroom js file.

---
+++ app/assets/javascripts/chatrooms.js
@@ -0,0 +1,27 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+
+  $(function() {
+    // Create a new client to connect to Faye
+    var fayeUrl = '/faye';
+    var client = new Faye.Client(fayeUrl);
+// Subscribe to the public channel. This allows connected users to receive chats.this the <p> tags are going to be used  to take.  Its a method that takes in two arguments.  Where it subscribes to and then the data or mesage.(http://faye.jcoglan.com/browser/subscribing.html). this is how people receive other peoples chats.
+    var public_subscription = client.subscribe('/messages/public', function(data) {
+      $('<p></p>').html(data.username + ": " + data.msg).prependTo('#chat_room');
+      if (data.scores) {
+        $(".userScores").html();
+      }
+    });
+
+     // Our own private channel. Allows an individual to receive messages on a private terminal.
+    // var private_subscription = client.subscribe('/messages/private/<%= session[:username] %>', function(data) {
+    //   $('<p></p>').addClass('private').html(data.username + ": " + data.msg).appendTo('#chat_room');
+    // });
+
+  });
+
+
+$( document ).ready(function() {
+
+});
